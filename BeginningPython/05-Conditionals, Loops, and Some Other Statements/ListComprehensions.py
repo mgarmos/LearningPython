33a40400263b7383589d497a561132be8eda7a71
mgarmos <mgarmos@gmail.com>
2018-04-05 22:18:49 +0200

    Project Euler

--- BeginningPython/05-Conditionals, Loops, and Some Other Statements/ListComprehensions.py
+++ BeginningPython/05-Conditionals, Loops, and Some Other Statements/ListComprehensions.py
@@ -8,3 +8,17 @@ a way similar to for loops and is actually quite simple.
 """
 square = [x * x for x in range(10)]
 print(square)
+
+#Multiplos de 3 al cuadrado
+mult = [x*x for x in range(10) if x % 3 == 0]
+print(mult)
+
+var1 = [(x, y) for x in range(3) for y in range(3)]
+print(var1)
+
+var2 = [(x, x**0.5 +y) for x in range(3) for y in range(3)]
+print('var2', var2)
+
+girls = ['alice', 'bernice', 'clarice']
+boys = ['chris', 'arnold', 'bob']
+print([b+'+'+g for b in boys for g in girls if b[0] == g[0]])
