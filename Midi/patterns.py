d63e87141553e64d20d3fe43674021179a7d489d
mgarmos <35372735+mgarmos@users.noreply.github.com>
2019-06-17 14:24:29 +0200

    Update patterns.py

--- Midi/patterns.py
+++ Midi/patterns.py
@@ -35,25 +35,55 @@ class A1(Pattern):
 
 class A2(Pattern):
     def __init__(self):
-        self.name = "DU DE"
+        self.name = "DU·DE"
         self.rithm = [0.5,0.5]
 		
 class A3(Pattern):
     def __init__(self):
-        self.name = "DU-Ta DE-Ta"
+        self.name = "DU-Ta·DE-Ta"
         self.rithm = [0.25, 0.25, 0.25, 0.25]
 
 class A4(Pattern):
     def __init__(self):
-        self.name = "DU DE-Ta"
+        self.name = "DU·DE-Ta"
         self.rithm = [0.5, 0.25, 0.25]
 
 class A5(Pattern):
     def __init__(self):
-        self.name = "DU-Ta DE"
+        self.name = "DU-Ta·DE"
         self.rithm = [0.25, 0.25, 0.5]
 
 class I1(Pattern):
     def __init__(self):
         self.name = "Inicio"
-        self.rithm = [1,1,1,1]					
+        self.rithm = [1,1,1,1]
+
+class B1(Pattern):
+    def __init__(self):
+        self.name = "DU"
+        self.rithm = [3]
+		
+class B2(Pattern):
+    def __init__(self):
+        self.name = "DU·DA·DI"
+        self.rithm = [1,1,1]
+
+class B3(Pattern):
+    def __init__(self):
+        self.name = "DU-Ta·DA·DI"
+        self.rithm = [0.5,0.5,1,1]
+
+class B4(Pattern):
+    def __init__(self):
+        self.name = "DU·DA-Ta·DI"
+        self.rithm = [1,0.5,0.5,1]
+
+class B5(Pattern):
+    def __init__(self):
+        self.name = "DU·DA·DI-Ta"
+        self.rithm = [1,1,0.5,0.5]
+
+class B6(Pattern):
+    def __init__(self):
+        self.name = "DU-Ta·DA-Ta·DI-Ta"
+        self.rithm = [0.5,0.5,0.5,0.5,0.5,0.5]
